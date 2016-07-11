119caaf00af04473a7024e14a1c1b46c065c026e
cyborg <kevinthecyborg@Kevins-MacBook-Pro.local>
2016-07-11 15:20:24 -0700

    minor tweaks

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thanks! We will be in touch soon. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
